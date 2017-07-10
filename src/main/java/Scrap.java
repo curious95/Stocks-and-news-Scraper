4dbe4068ba92f60e8e6a66d6b8fe4d9fe7ed2587
kamalpradhan95 <kamal.pradhan@aptusdatalabs.com>
2017-07-10 19:45:00 +0530

    Fetching Headline from google news

--- src/main/java/Scrap.java
+++ src/main/java/Scrap.java
@@ -1,8 +1,28 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  * Created by aptus on 10/7/17.
  */
 public class Scrap {
     public static void main(String[] args) {
-        System.out.println("RUN");
+
+        try {
+
+            Document doc = Jsoup.connect("https://news.google.com/news/headlines/section/topic/NATION.en_in/India?ned=in&hl=en-IN").userAgent("Mozilla").data("name", "jsoup").get();
+
+            Elements links = doc.getElementsByClass("nuEeue hzdq5d ME7ew");
+
+            for (Element link : links) {
+                // get the value from the href attribute
+                System.out.println("\nlink: " + link.attr("href"));
+                System.out.println("text: " + link.text());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
