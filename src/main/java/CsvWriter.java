a9db04ad78485f32e3ae8efe4bc8da330dc2c41d
curious95 <kamalpradhan95@outlook.com>
2017-07-14 14:07:37 +0530

    Economy news completed CSV generated

--- src/main/java/CsvWriter.java
+++ src/main/java/CsvWriter.java
@@ -1,9 +1,58 @@
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by aptus on 14/7/17.
  */
 public class CsvWriter {
-    private static final Object[] FILE_HEADER = {"news_id", "time_id", "news", "news_category",
+    private static final Object[] FILE_HEADER = {"news_id", "time_id","timestamp" ,"news", "news_category",
                 "news_link", "BSE_points", "NSE_points", "BSE_G1", "BSE_G2", "BSE_G3", "BSE_G4",
                 "BSE_G5", "BSE_L1", "BSE_L2", "BSE_L3", "BSE_L4", "BSE_L5", "NSE_G1", "NSE_G2",
                 "NSE_G3", "NSE_G4", "NSE_G5", "NSE_L1", "NSE_L2", "NSE_L3", "NSE_L4", "NSE_L5"};
+
+    private static final String NEW_LINE_SEPARATOR = "\n";
+    private static final CSVFormat csvFileFormat = CSVFormat.DEFAULT.withRecordSeparator(NEW_LINE_SEPARATOR);
+
+    FileWriter fileWriter = null;
+    CSVPrinter csvFilePrinter = null;
+
+    static boolean head_status = false;
+
+
+    void fileWriter(ArrayList<List> newsRecords){
+        try{
+
+            if(head_status!=true) {
+                fileWriter = new FileWriter("test.csv");
+
+
+                csvFilePrinter = new CSVPrinter(fileWriter, csvFileFormat);
+                csvFilePrinter.printRecord(FILE_HEADER);
+            }
+
+            for(List l1 : newsRecords){
+
+                csvFilePrinter.printRecord(l1);
+                //System.out.println(l1.toString());
+            }
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try {
+                fileWriter.flush();
+                fileWriter.close();
+                csvFilePrinter.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
+    }
 }
--- src/main/java/Scrap.java
+++ src/main/java/Scrap.java
@@ -3,12 +3,19 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Curious95 on 10/7/17.
  */
 public class Scrap {
+
+    public static List newsRecord = new ArrayList();
+    public static ArrayList<List> Allrecords = new ArrayList<List>();
+
     public static void main(String[] args) {
 
         StocksBSE se = new StocksBSE();
@@ -28,16 +35,45 @@ public class Scrap {
         HashMap<String, String> neBusiness = nes.fetchBusiness();
         HashMap<String, String> neCountry = nes.fetchCountry();
 
+        CsvWriter writeRecord = new CsvWriter();
 
-        neEconomy.entrySet().forEach(item->{
+        /*neEconomy.entrySet().forEach(item->{
+            System.out.println(item.getKey());
+        });*/
 
-        });
+        Scrap sc = new Scrap();
 
+        for (Map.Entry<String, String> entry : neEconomy.entrySet()) {
 
-        System.out.println(hash.generateHash(new java.util.Date().toString()));
+            newsRecord = new ArrayList();
 
+            newsRecord.add(hash.generateHash(entry.getKey()));
+            newsRecord.add(hash.generateHash(new java.util.Date().toString()));
+            newsRecord.add(new java.util.Date());
+            newsRecord.add(entry.getKey());
+            newsRecord.add("Economy");
+            newsRecord.add(entry.getValue());
+            newsRecord.add(seIndex);
+            newsRecord.add(neIndex);
 
+            seGainers.entrySet().forEach(gn -> {
+                newsRecord.add(gn.getKey());
+            });
+            seLosers.entrySet().forEach(gn -> {
+                newsRecord.add(gn.getKey());
+            });
+            neGainers.entrySet().forEach(gn -> {
+                newsRecord.add(gn.getKey());
+            });
+            neLosers.entrySet().forEach(gn -> {
+                newsRecord.add(gn.getKey());
+            });
 
+            Allrecords.add(newsRecord);
+        }
+
+        writeRecord.fileWriter(Allrecords);
 
     }
+
 }
